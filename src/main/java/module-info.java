0ed90e652f522dffffeddd24eb10309618a186d6
florianBurdairon <florian.burdairon@gmail.com>
2024-05-10 18:58:26 +0200

    fix javafx not working with intellij

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module tp.optimisation {
+    requires javafx.controls;
+    exports tp.optimisation;
+    exports tp.optimisation.utils;
+    exports tp.optimisation.rendering;
+    exports tp.optimisation.neighbours;
+    exports tp.optimisation.metaheuristics;
+}
